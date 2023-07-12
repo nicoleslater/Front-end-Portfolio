bfb0aa7f1f3acee7497c327fa2180b9f9e630472
Nicole Slater <nicoleslater@pursuit.org>
2023-07-11 21:34:06 -0400

    JS

--- Home/indexHome.js
+++ Home/indexHome.js
@@ -1,54 +1,35 @@
-const NewsAPI = require('newsapi');
-const newsapi = new NewsAPI('248076a179b4446ca60d2fbbe2fbbb60');
-// To query /v2/top-headlines
-// All options passed to topHeadlines are optional, but you need to include at least one of them
-newsapi.v2.topHeadlines({
-  sources: 'bbc-news,the-verge',
-  q: 'bitcoin',
-  category: 'business',
-  language: 'en',
-  country: 'us'
-}).then(response => {
-  console.log(response);
-  /*
-    {
-      status: "ok",
-      articles: [...]
-    }
-  */
-});
-// To query /v2/everything
-// You must include at least one q, source, or domain
-newsapi.v2.everything({
-  q: 'bitcoin',
-  sources: 'bbc-news,the-verge',
-  domains: 'bbc.co.uk, techcrunch.com',
-  from: '2017-12-01',
-  to: '2017-12-12',
-  language: 'en',
-  sortBy: 'relevancy',
-  page: 2
-}).then(response => {
-  console.log(response);
-  /*
-    {
-      status: "ok",
-      articles: [...]
-    }
-  */
-});
-// To query sources
-// All options are optional
-newsapi.v2.sources({
-  category: 'technology',
-  language: 'en',
-  country: 'us'
-}).then(response => {
-  console.log(response);
-  /*
-    {
-      status: "ok",
-      sources: [...]
-    }
-  */
-});
+
+async function getNews(){
+    await fetch('https://newsapi.org/v2/everything?q=Apple&from=2023-07-12&sortBy=popularity&apiKey=248076a179b4446ca60d2fbbe2fbbb60')
+    .then(data => data.json())
+    .then(response => {
+        console.log(response.results);
+
+        for(let i = 0; i < response.results.length; i++){
+            console.log(response.results[i].title);
+            const output = document.getElementById('output');
+
+            try {
+                output.innerHTML += `
+                <div class="card">
+                <div class="card-body">
+                <img src="${response.results[i]['media'][0][media-metadata]}" />
+                <h2>${response.results[i].title}</h2>
+                <div class="card-text">
+                <p>${response.results[i].abstract}</p>
+                </div>
+                </div>
+                </div>
+                <br>`
+            }
+            catch(err){
+                console.log(err);
+
+            }
+        
+        
+        }
+    })}
+
+
+
