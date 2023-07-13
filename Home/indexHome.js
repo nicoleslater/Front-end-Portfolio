16fb9e477c3b7579ab73826317045e5d56d7c91c
Nicole Slater <nicoleslater@pursuit.org>
2023-07-12 20:57:25 -0400

    Functionality in JS

--- Home/indexHome.js
+++ Home/indexHome.js
@@ -10,40 +10,42 @@ fetch(request)
 .then(function(response){
     console.log(response.json());
 })
- function getNews(){
-    fetch('https://newsapi.org/v2/everything?q=Apple&from=2023-07-12&sortBy=popularity&apiKey=248076a179b4446ca60d2fbbe2fbbb60')
-    .then(data => data.json())
-    .then(response => {
-        console.log(response.results);
 
-        for(let i = 0; i < response.results.length; i++){
-            console.log(response.results[i].title);
-            const output = document.getElementById('output');
 
-            try {
-                output.innerHTML += `
-                <div class="card">
-                <div class="card-body">
-                <img src="${response.results[i]}" />
-                <h2>${response.results[i].title}</h2>
-                <div class="card-text">
-                <p>${response.results[i].abstract}</p>
-                </div>
-                </div>
-                </div>
-                <br>
-                `
-            }
-            catch(err){
-                console.log(err);
+//  function getNews(){
+//     fetch('https://newsapi.org/v2/everything?q=Apple&from=2023-07-12&sortBy=popularity&apiKey=248076a179b4446ca60d2fbbe2fbbb60')
+//     .then(data => data.json())
+//     .then(response => {
+//         console.log(response.results);
 
-            }
+//         for(let i = 0; i < response.results.length; i++){
+//             console.log(response.results[i].title);
+//             const output = document.getElementById('output');
+
+//             try {
+//                 output.innerHTML += `
+//                 <div class="card">
+//                 <div class="card-body">
+//                 <img src="${response.results[i]}" />
+//                 <h2>${response.results[i].title}</h2>
+//                 <div class="card-text">
+//                 <p>${response.results[i].abstract}</p>
+//                 </div>
+//                 </div>
+//                 </div>
+//                 <br>
+//                 `
+//             }
+//             catch(err){
+//                 console.log(err);
+
+//             }
         
         
-        }
+//         }
         
-    })}
-    getNews()
+//     })}
+//     getNews()
 
 
 
