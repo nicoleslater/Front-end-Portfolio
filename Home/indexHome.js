cd5c093834c235682042afa436317aae541e6a53
Nicole Slater <nicoleslater@pursuit.org>
2023-07-14 20:12:54 -0400

    Creating Functions in Javascript

--- Home/indexHome.js
+++ Home/indexHome.js
@@ -1,15 +1,45 @@
 https://newsapi.org/v2/everything?q=Apple&from=2023-07-12&sortBy=popularity&apiKey=248076a179b4446ca60d2fbbe2fbbb60
 
+const newsQuery = document.getElementById("newsQuery");
+const newsdetails = document.getElementById("newsdetails");
+const newsType = document.getElementById("newsType");
+const generalBtn = document.getElementById("general");
+const businessBtn = document.getElementById("business");
+const sportsBtn = document.getElementById("sports");
+const technologyBtn = document.getElementById("technology");
+const entertainmenttBtn = document.getElementById("entertainment");
+const searchBtn = document.getElementById("searchBtn");
 
 
-let url = 'https://newsapi.org/v2/top-headlines?' +
-          'country=us&' +
-          'apiKey=248076a179b4446ca60d2fbbe2fbbb60';
-let req = new Request(url);
-fetch(req)
-    .then(function(response) {
-        console.log(response.json());
-    })
+
+generalBtn.addEventListener("click", function(){
+
+})
+businessBtn.addEventListener("click", function(){
+    
+})
+sportsBtn.addEventListener("click", function(){
+    
+})
+technologyBtn.addEventListener("click", function(){
+    
+})
+entertainmentBtn.addEventListener("click", function(){
+    
+})
+searchBtn.addEventListener("click", function(){
+    
+})
+
+
+// let url = 'https://newsapi.org/v2/top-headlines?' +
+//           'country=us&' +
+//           'apiKey=248076a179b4446ca60d2fbbe2fbbb60';
+// let req = new Request(url);
+// fetch(req)
+//     .then(function(response) {
+//         console.log(response.json());
+//     })
 
 // const url = 'https://newsapi.org/v2/top-headlines?' +
 //           'country=us&' +
