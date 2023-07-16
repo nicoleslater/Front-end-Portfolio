57b7c66e3ef4bba9d6904e3370293f66eb4d9dcd
Nicole Slater <nicoleslater@pursuit.org>
2023-07-16 01:22:50 -0400

    Error Message in Email

---
+++ Contacts/contact.js
@@ -0,0 +1,18 @@
+// const name = document.getElementById('name');
+// const email = document.getElementById('email');
+// const subject = document.getElementById('subject');
+
+// btn.addEventListener('click', function(e){
+//     e.preventDefault()
+
+//     if(name.value == "" && email.value == "" && subject.value == ""){
+//         alert("Fill The Form Please");
+//     } else {
+//         const newRow = document.createElement('section')
+//         const newName = document.createElement('div')
+//         newName.innerHTML = name.value
+//         newRow.appendChild(newName);
+//     }
+// });
+
+
